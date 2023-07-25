cd6359821e216b275375299d3efad2fa26a2093c
Tyler Jensen <ronsper@gmail.com>
2023-07-24 20:43:04 -0700

    Removed the big vars

---
+++ Hub/deviceStates.js
@@ -0,0 +1,38 @@
+export const deviceStates = {
+    1: {
+        deviceID: 1,
+        OK: false,
+        desiredValveState: {
+            A: false,
+            B: false,
+            C: false
+        }
+    },
+    2: {
+        deviceID: 2,
+        OK: false,
+        desiredValveState: {
+            A: false,
+            B: false,
+            C: false
+        }
+    },
+    3: {
+        deviceID: 3,
+        OK: false,
+        desiredValveState: {
+            A: false,
+            B: false,
+            C: false
+        }
+    },
+    4: {
+        deviceID: 4,
+        OK: false,
+        desiredValveState: {
+            A: false,
+            B: false,
+            C: false
+        }
+    }
+}
